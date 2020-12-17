c4817720334ed6f552f1274e47356cb91bb98b94
Georgiy Efimov <georgiy.efimov96@gmail.com>
2020-12-17 11:42:53 +0100

    Commit van Flatlist
    MapDetailsScreen moet nog afgwerkt worden

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useEffect, useState} from 'react';
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
-import rca from "rainbow-colors-array";
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, ActivityIndicator, SectionList } from 'react-native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
@@ -9,8 +8,9 @@ import { FlatList } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import MapView, {Marker} from 'react-native-maps';
 import { set } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
 
-const getZwembadenFromApiAsync = async () => {
+/*const getZwembadenFromApiAsync = async () => {
   try {
     let response = await fetch(
       'https://opendata.arcgis.com/datasets/b760e319033841348469bacb34c5e259_644.geojson'
@@ -20,18 +20,115 @@ const getZwembadenFromApiAsync = async () => {
   } catch (error) {
     console.error(error);
   }
+};*/
+
+/*const ListView = (props,{navigation}) =>{
+
+  //let navigation = props.navigation;
+
+  return(
+    <View style = {{backgroundColor: "grey", padding: 20, margin: 10}}>
+      <Text>{props.name}</Text>
+      <Button title="Details" onPress={() =>navigation.navigate("MapDetails")}/>
+    </View>
+  );
+}*/
+
+export const ListScreen = ({navigation}) => {
+  const [mapList, setList] = useState([]);
+
+  const loadList = async () =>{
+    try{
+      let response = await fetch('https://opendata.arcgis.com/datasets/b760e319033841348469bacb34c5e259_644.geojson');
+
+      let json = await response.json();
+
+
+      setList(json.features);
+    }catch(error){
+
+    }
+  }
+
+  const renderItem = ({item}) =>{
+    return (
+
+        <View style = {{backgroundColor: "grey", padding: 20, margin: 10}}>
+        <Text>{item.properties.naam}</Text>
+        <Button title="Details" onPress={() =>navigation.navigate('Details')}/>
+      </View>);
+      
+    //<ListView name = {item.properties.naam}/>);
+    
+  }
+
+  const keyExtractor = (item) => item.properties.id;
+
+  useEffect(()=>{
+    loadList();
+  },[])
+
+  return(
+    <View style={styles.container}>
+
+    <FlatList
+    data = {mapList}
+    renderItem = {renderItem}
+    keyExtractor = {keyExtractor}/>
+    
+    </View>
+  );
+
+
+
 };
 
 
 
 
+//<Stack.Screen name="Listscreen" component={ListScreen} options={{title:"Zwembaden lijst"}}/>
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MapScreenStack}/>
+      <Stack.Screen name="Details" component={MapDetailsScreen} />
+    </Stack.Navigator>
+    </NavigationContainer>
+   
+
+  );
+}
+
+const Stack = createStackNavigator();
+export const MapScreenStack = () =>{
+  return(
+    
+      <Tab.Navigator>
+        
+        <Tab.Screen name="Map" component={MapScreen}/>
+        <Tab.Screen name="List" component={ListScreen}/>
+      </Tab.Navigator>
+    
+    
+  );
+}
+
+export const MapDetailsScreen = ({navigation, route}) =>{
+  const {itemId} = route.params;
+  return(
+    <Text>BlabLa</Text>
+
+  );
+}
+
+export const MapScreen = () =>{
+  return(
+    <MapView style={{flex:1}}/>
   );
 }
 
@@ -39,7 +136,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
   },
 });
